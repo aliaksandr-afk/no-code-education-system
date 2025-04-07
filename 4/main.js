20d8f4f7706f3d04727a470e4bd81cfac3103bb5
Aliaksandr Baranau <aleks.feedback@gmail.com>
2025-04-07 13:03:10 +0300

    added script video

--- 4/main.js
+++ 4/main.js
@@ -5,3 +5,15 @@ const award = document.querySelector(".award__images");
 award.addEventListener("click", () => {
   document.querySelector(".award").remove();
 });
+// Video Section 1
+
+const btnClickVideo = document.getElementById("content__btn2");
+
+const video = document.getElementById("content__videos");
+function openVideo() {
+  video.play();
+}
+
+btnClickVideo.addEventListener("click", () => {
+  openVideo();
+});
